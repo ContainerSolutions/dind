64a6b563321b34f2536476ab84cd7cc4ba453c58
Lukasz Guminski <lguminski@gmail.com>
2015-07-30 19:11:59 +0200

    support for LOG=file env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 export HTTP_PORT=7777
-{
+([[ $LOG = file ]] && { exec > /var/log/entrypoint.log; exec 2>&1; } # redirecting output to file if requested
   wrapdocker &
   while ! docker ps > /dev/null
     do sleep 1 # wait for docker service to get ready
   done
   /bin/logspout &
-} >/var/log/entrypoint.log 2>&1
+)
 while ! (stdbuf -o0 curl -s localhost:${HTTP_PORT}/logs | stdbuf -o0 tail -n+2)
   do sleep 1 # wait for logspout to open http port
 done
