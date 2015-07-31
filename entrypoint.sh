6781d7a9fb67dfd8d2e284261367964f0bfa2cf7
Lukasz Guminski <lguminski@gmail.com>
2015-07-31 14:01:11 +0200

    reformatting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-export HTTP_PORT=7777
+export HTTP_PORT=${HTTP_PORT:-7777}
 ([[ $LOG = file ]] && { exec > /var/log/entrypoint.log; exec 2>&1; } # redirecting output to file if requested
-  wrapdocker &
-  while ! docker ps > /dev/null
-    do sleep 1 # wait for docker service to get ready
-  done
-  /bin/logspout &
+    wrapdocker &
+    # wait for docker service to respond
+    while ! docker ps > /dev/null 2>&1
+        do sleep 1
+    done
+    # start logspout
+    /bin/logspout &
 )
+# stream logspout output
 while ! (stdbuf -o0 curl -s localhost:${HTTP_PORT}/logs | stdbuf -o0 tail -n+2)
-  do sleep 1 # wait for logspout to open http port
+    do sleep 1 # wait for logspout to open http port
 done
